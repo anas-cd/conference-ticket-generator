fb26ba0c013f41fef57fe4a90eb329dd226701be
Anas Shamlan <anas.cd.97@gmail.com>
2025-01-27 19:49:39 +0300

    project set up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,49 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+
+    <h1>New To VPS!</h1>
+    <sub>^_^</sub>
+
+    <!-- template provided by Frontend Mentor -->
+    <!-- Form starts -->
+
+    <!-- Your Journey to Coding Conf 2025 Starts Here! Secure your spot at next year's biggest coding
+    conference. Upload Avatar Drag and drop or click to upload Upload your photo (JPG or PNG, max
+    size: 500KB). Full Name Email Address example@email.com GitHub Username @yourusername Generate
+    My Ticket -->
+
+    <!-- Form ends -->
+
+    <!-- Generated tickets starts -->
+
+    <!-- Congrats,'FULL NAME'! Your ticket is ready. We've emailed your ticket to 'EMAIL'
+    and will send updates in the run up to the event. Coding Conf Jan 31, 2025 / Austin, TX -->
+
+    <!-- Generated tickets ends -->
+
+    <!-- <div id="attr" class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. Coded
+      by <a href="#">Your Name Here</a>.
+    </div> -->
   </main>
 </template>
+
+<style lang="scss">
+main {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  color: var(--orange-500);
+
+  h1 {
+    font-weight: 800;
+    font-size: xx-large;
+  }
+  sub {
+    font-size: xx-large;
+  }
+}
+</style>
